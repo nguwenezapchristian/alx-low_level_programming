f23aa420d699612ff80871a6a797ecdad058267e
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-16 14:09:23 -0800

    size of diff. types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@ int main(void)
 	/* variable initialization */
 	int i;
 	char c;
-	double d;
-	short s;
-	long l;
+	long int s;
+	long long int l;
+	float f;
 	/* statements */
 	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of a double: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a short: %lu byte(s)\n", (unsigned long)sizeof(s));
-	printf("size of a long: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(s));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	/* return 0 */
 	return (0);
 }
