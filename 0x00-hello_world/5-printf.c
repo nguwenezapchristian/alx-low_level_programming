f7125171aef9a41b5a7d8c3295ae400512515b81
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-16 11:33:02 -0800

    print text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- * print text 
- * return 0
-*/
+/* display a text*/
 int main(void)
+/* main: print text */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/* return 0 */
 	return (0);
 }
