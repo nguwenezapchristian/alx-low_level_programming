9bfaa0762070419e48ed9f9d409bffd224c1f91c
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-16 12:56:25 -0800

    using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* display text */
+int main(void) /*function main return integer */
+{
+	/* assigning a variable */
+	char myStr[] = "\"Programming is like building a multilingual puzzle\n";
+	/* statement */
+	puts(myStr);
+	/* return 0 */
+	return (0);
+}
