4c049123a0fc8d1578bbcbce07001b57611d7f42
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-17 13:01:55 -0800

    lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'lowercase in reverse'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* decaralation */
+	char lo;
+
+	for (lo = 'z'; lo >= 'a'; lo--)
+	{
+		putchar(lo);
+	}
+	return (0);
+}
