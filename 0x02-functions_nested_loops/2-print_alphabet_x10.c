ec5d569329f22a23d5205bfc371d217472b3d56f
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-21 12:38:52 -0800

    10 lines of alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - entry point
  * description: 'print 10 lines of alphabet'
- * Return: Always 0 
+ * Return: Always 0
  */
 void print_alphabet_x10(void)
 {
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 
 	while (n < 10)
 	{
-		_putchar('\n');
 		a = 'a';
 	while (a <= 'z')
 	{
 		_putchar(a);
 		a++;
 	}
-		n++;
+	_putchar('\n');
+	n++;
 	}
 }
