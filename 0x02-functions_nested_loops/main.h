cb01de91bd08db1b3b515343a068e84361574f15
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-21 07:52:50 -0800

    my library

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,7 @@
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -11,9 +9,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-/**
- * printword - print a word
- * description: 'write a word'
- * Return: Always 0 (success)
- */
-int printword(void);
