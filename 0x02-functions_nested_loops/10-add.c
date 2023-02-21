bd6b6e71b94ec0a969b3487e515da70a989c3241
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-21 15:50:26 -0800

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - entry point
+ * @num1: integer one
+ * @num2: integer two
+ * description: 'adds two number'
+ * Return: Always return sum
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+	return (sum);
+}
