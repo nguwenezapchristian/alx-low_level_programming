8bbdaf21a3e4215a845b2c10ef678fac393c7ed3
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-21 07:53:33 -0800

    print _printchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,29 @@
 #include "main.h"
 /**
- * main - Entry point
+ * printword - Entry point
  * description: 'prints a word'
+ * Return: Always 0 (success)
+ */
+int printword(void)
+{
+	char word[] = "_putchar";
+	int i = 0;
+
+	while (word[i] != '\0')
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
+/**
+ * main - entry point
+ * description: 'print a word'
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	printword();
 	return (0);
-	int printword(void)
-{
-        char word[] = "_putchar";
-        int i = 0;
-
-        while (word[i] != '\0')
-        {
-                _putchar(word[i]);
-                i++;
-        }
-        _putchar('\n');
-        return (0);
-}
 }
