3ec3789222c5f1f05210199db483aef190fa0b0b
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-21 07:14:05 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,4 +8,17 @@ int main(void)
 {
 	printword();
 	return (0);
+	int printword(void)
+{
+        char word[] = "_putchar";
+        int i = 0;
+
+        while (word[i] != '\0')
+        {
+                _putchar(word[i]);
+                i++;
+        }
+        _putchar('\n');
+        return (0);
+}
 }
