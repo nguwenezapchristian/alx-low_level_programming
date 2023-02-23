3a16b79c6302980f4b946cb51a9c6bd59446f84b
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-23 12:08:41 -0800

    print digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ * description: 'print digit numbers'
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
