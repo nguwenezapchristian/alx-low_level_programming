843bd34da33d722f1345c9072e404dc63a503bb9
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-23 11:46:20 -0800

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - entry point
+ * @a: number
+ * @b: number to multiply with
+ * description: 'multiply two numbers'
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
