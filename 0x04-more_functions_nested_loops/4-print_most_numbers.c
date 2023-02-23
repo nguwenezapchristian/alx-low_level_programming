e06a93d1b5488c507bd6bf19672e23ce1d887a44
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-23 12:28:59 -0800

    digit except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - entry point
+ * description: 'print digit numbers excluding 2 and 4'
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if (i != 50 || i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
