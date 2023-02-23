c76c8fa1206c8cc064446e3138af034e99fbbaed
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-23 14:42:25 -0800

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ * @n: number of ' _ '
+ * description: 'print line'
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		x = 0;
+
+		while (n >= x)
+		{
+			_putchar('_');
+			x++;
+		}
+		_putchar('\n');
+	}
+}
