7d10fdf662a7defe5bdd37e7e1b2c888032d3f8c
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-02-24 14:25:31 -0800

    print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,11 @@ void print_diagonal(int n)
 		{
 			_putchar(' ');
 		}
-		_putchar(92);
+		if (z <= (n - x))
+		{
+			_putchar(92);
+			_putchar('\n');
+		}
 		if (n <= 0)
 		{
 			_putchar('\n');
