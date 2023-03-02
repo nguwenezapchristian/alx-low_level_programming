f4cde6df286573cc27635748817856d5ed57d316
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-03-02 00:42:29 -0800

    header file containing prototypes

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef main_h
+#define main_h
+#include <unistd.h>
+
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+#endif
