314949d7cd9fafeeca95198006b4580daeeb5a74
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-03-07 12:05:46 -0800

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int l1 = 0, a;
+	int a;
 
-	for (a = 0; *(dest + a) != '\0'; a++)
-	{
-		l1++;
-	}
 	for (a = 0; a < n; a++)
 	{
-		*(dest + (l1 + a)) = *(src + a);
+		*(dest + a) = *(src + a);
 	}
 	return (dest);
 }
