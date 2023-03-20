2206d00422c81be46dc8c95bd6f7788392bba49b
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-03-20 15:14:19 -0700

    header file contain prototypes and struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,12 @@
 #define DOG_H
 
 /**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
  * struct dog - define a new type struct dog
  * @name: dog name
  * @age: dog age
@@ -14,5 +20,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
