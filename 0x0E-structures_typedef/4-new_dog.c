b2dbdd910d11ee24c2ee451620e3286c38fe0225
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-03-20 16:50:54 -0700

    a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ * return: new dog
+ */
+
+dog_t *new_dog(char *name, float  age, char *owner)
+{
+	dog_t *d;
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+		return(d);
+	}
+}
