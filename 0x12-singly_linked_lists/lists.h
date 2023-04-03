8dd064d2468ed6e1745a4ad1f63c52e3020250fc
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-04-03 04:47:31 -0700

    fixed betty coding style

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -22,5 +22,7 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 
 list_t *add_node(list_t **head, const char *str);
+
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
