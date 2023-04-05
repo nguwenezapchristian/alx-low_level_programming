805aabef8d72175e9a0d3ad787626dbe3946c501
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-04-05 02:28:24 -0700

    added a prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,5 +25,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 
 list_t *add_node_end(list_t **head, const char *str);
+
+void free_list(list_t *head);
 
 #endif
