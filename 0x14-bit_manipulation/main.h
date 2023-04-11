76a2a367987060225cafd34e7aa393012df70f67
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-04-11 11:14:11 -0700

    header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,11 +2,10 @@
 #define MAIN_H
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
 
-unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 int _pow_recursion(int x, int y);
-void rev_string(const char *s);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
