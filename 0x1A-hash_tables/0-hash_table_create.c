a2d9c1d21d3483f2b0a2746a56a85382125bfc61
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-06-10 14:45:20 -0700

    fixed bettystyle errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *new_hash_t = (hash_table_t*) malloc(sizeof(hash_table_t));
+	hash_table_t *new_hash_t = malloc(sizeof(hash_table_t));
 
 	new_hash_t->size = size;
-	new_hash_t->array = (hash_node_t**) calloc(new_hash_t->size, sizeof(hash_node_t*));
+	new_hash_t->array = calloc(new_hash_t->size, sizeof(hash_node_t *));
 	for (i = 0; i < new_hash_t->size; i++)
 		new_hash_t->array[i] = NULL;
-	return new_hash_t;
+	return (new_hash_t);
 }
