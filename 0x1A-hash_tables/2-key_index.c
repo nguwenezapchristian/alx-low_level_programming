70480e66d9a610f1ea4b18b0a609633363c7f0e2
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-06-10 14:19:35 -0700

    returns the index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: key
+ * @size: size of the array of the hash table
+ * Return: index of a key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index, hash_value;
+
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
+	return index;
+}
