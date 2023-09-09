6b44025506cb7b012b6e2467aff6d35acbfc1492
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-09-09 17:29:44 +0200

    fixed memory allocation problem

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(new_table));
+	hash_table_t *new_table = malloc(sizeof(new_table));
 
 	if (new_table == NULL)
 	{
+		free(new_table);
 		return (NULL);
 	}
 	new_table->size = size;
