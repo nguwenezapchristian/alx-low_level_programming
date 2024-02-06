1e6ac472df4fd31badb6c95d81729be6f3475762
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-02-06 14:00:15 +0200

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
 #ifndef _SEARCH_ALOGOS_
-#define _SEARCH_ALGOS_
+#define _SEARCH_ALOGOS_
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+
 #endif
